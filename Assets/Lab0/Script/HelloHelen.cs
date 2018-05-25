8c114f08b29696076e16f4a48c3893a509b89cb4
ImgyuKim <kimimgo@gmail.com>
2018-05-25 17:39:06 +0900

    first commit

---
+++ Assets/Lab0/Script/HelloHelen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloHelen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("this is first push");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
