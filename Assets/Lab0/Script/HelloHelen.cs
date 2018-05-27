a9f7795714aff9bacc5a4372334c3c32fd47ef71
HyebinKim <qls5191@kaist.ac.kr>
2018-05-27 18:36:47 +0900

    hello imgye 추가

--- Assets/Lab0/Script/HelloHelen.cs
+++ Assets/Lab0/Script/HelloHelen.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HelloHelen : MonoBehaviour {
 
+    public Text tt;
+    
 	// Use this for initialization
 	void Start () {
         Debug.Log("this is first push");
-	}
+        tt.text = "hello imgyu";
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        tt.text = "hello imgyu";
+    }
 }
